4c0bb678a801beed80214cfdd1ab0e519fdffc5a
zhaiHS <zhaihaisheng9612@qq.com>
2019-11-05 08:46:04 +0800

    :swimmer: append test

--- src/test/java/bean/BeanTest.java
+++ src/test/java/bean/BeanTest.java
@@ -1,6 +1,11 @@
 package bean;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import di.BasicDI;
+import di.ListDI;
+import di.MapDI;
+import di.PropertiesDI;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,14 +14,67 @@ import pojo.User;
 public class BeanTest {
     ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
     @Test
-    public void noArgumentsInstantiationTest() {
+    public void noArgumentsInstantiationBeanTest() {
         User bean = (User) applicationContext.getBean("user");
         System.out.println(bean);
     }
 
     @Test
-    public void staticFactoryBeanTest() {
+    public void staticFactoryInstantiationBeanTest() {
         User bean = (User) applicationContext.getBean("user4StaticFactory");
+        System.out.println(bean);
+    }
+
+    @Test
+    public void dynamicFactoryInstantiationBeanTest() {
+        User bean = (User) applicationContext.getBean("user4DynamicFactory");
+        System.out.println(bean);
+    }
+
+    @Test
+    public void constructorDITest() {
+        User bean = (User) applicationContext.getBean("user4Constructor");
+        System.out.println(bean);
+    }
+
+    @Test
+    public void setDITest() {
+        User bean = (User) applicationContext.getBean("user4Set");
+        System.out.println(bean);
+    }
+
+    @Test
+    public void setDIWithPTest() {
+        User bean = (User) applicationContext.getBean("user4p");
+        System.out.println(bean);
+    }
+    @Test
+    public void basicDITest() {
+        BasicDI bean = applicationContext.getBean(BasicDI.class);
+        System.out.println(bean);
+    }
+
+    @Test
+    public void listDITest() {
+        ListDI bean = applicationContext.getBean(ListDI.class);
+        System.out.println(bean);
+    }
+
+    @Test
+    public void mapDITest() {
+        MapDI bean = applicationContext.getBean(MapDI.class);
+        System.out.println(bean);
+    }
+
+    @Test
+    public void propertiesDITest() {
+        PropertiesDI bean = applicationContext.getBean(PropertiesDI.class);
+        System.out.println(bean);
+    }
+
+    @Test
+    public void DatasourceTest() {
+        ComboPooledDataSource bean = applicationContext.getBean(ComboPooledDataSource.class);
         System.out.println(bean);
     }
 }
